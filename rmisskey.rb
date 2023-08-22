5ce6bf7b7ee03439e61d43f8bc9b6d0dc3eaf979
longicorn <longicorn.c@gmail.com>
2023-08-22 11:52:25 +0900

    Update post

--- rmisskey.rb
+++ rmisskey.rb
@@ -59,21 +59,35 @@ options.each do |key, value|
   when :info
     pp misskey.i
   when :post, :reply
+    text = ''
+
+    # get text
     if options[:file].nil?
-      $stderr.puts opt.help
-      exit 1
-    end
-
-    if options[:file].size <= 0
-      $stderr.puts opt.help
-      exit 1
-    end
-    text = File.read(options[:file]).chomp
-
-    if options[:reply]
-      misskey.note_create(text, reply_id: options[:reply_id])
+      if ARGV.empty?
+        rs, _ = IO.select([$stdin], [], [], 0.1)
+        exit if rs.nil?
+        text = $stdin.gets if rs
+      else
+        text = ARGV.shift
+      end
     else
-      misskey.note_create(text)
+      text = File.read(options[:file])
+    end
+    if text.size <= 0
+      $stderr.puts opt.help
+      exit 1
+    end
+    text = text.chomp
+
+    ret = nil
+    if options[:reply]
+      ret = misskey.note_create(text, reply_id: options[:reply_id])
+    else
+      ret = misskey.note_create(text)
+    end
+    unless ret
+      puts 'Post failed'
+      exit 1
     end
   end
 end
