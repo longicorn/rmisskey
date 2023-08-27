76611be2cf372c010bf5ef1c93156d0125fe1875
longicorn <longicorn.c@gmail.com>
2023-08-27 20:31:53 +0900

    Bugfix Mcache

--- mcache.rb
+++ mcache.rb
@@ -23,13 +23,15 @@ class Mcache
 
     def read(type, key, timeout: nil)
       path = target_path(type, key)
-      if path && File.exist?(path)
-        s = File::Stat.new(path)
-        return JSON.load(File.read(path)) if Time.now <= s.mtime + timeout
+      return unless path && File.exist?(path)
+
+      s = File::Stat.new(path)
+      if timeout && Time.now <= s.mtime + timeout
         # delete timeout cache
         delete(path)
+        return
       end
-      nil
+      JSON.parse(File.read(path))
     end
 
     def write(type, key, data)
@@ -37,7 +39,7 @@ class Mcache
       path = target_path(type, key)
       File.open(path, 'w') do |f|
         data = data.to_json unless data.is_a?(String)
-        f.write(data.to_json)
+        f.write(data)
       end
     end
 
